2e1ebce81384dd305350f9accf920884f1854623
BT_4N2_04 <TracyZ14@nycstudents.net>
2024-02-28 14:07:39 -0500

    Created a list of students

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,33 @@
+import java.util.ArrayList;
 public class Main
 {
     public static void main(String[][] args)
     {
-        String[][] seating = {{"Artyom Simonyan", "Haroon Awan", "Veronica Joseph", "Tracy Zhou", null, "Nafisa Patwary", "Dariusz Smola", "Tian Ying Chen", "Shakib Amin", "Noelle Wu", "Adib Miah", "Jason Teo"}, {"Siyam Siddique", "Leandra Boodram", null, "Rafael Bui", "Xin Chen", "Farian Faruqee"}, {"Irsal Siam", "Stanley Lee", "Sharon Lin", null, "Ricky Jiang", "Yanbo Wang"}, {null, null}, {"Christian Wan", "Brandon Yeung", "Nikita Manannikov", "Naya Antaki"}, {null, "Enamul Muttakin", "Jack Ni", "Ryan Chen"}};
+        String[][] seating = {{"Artyom Simonyan", "Haroon Awan", "Veronica Joseph", "Tracy Zhou", "", "Nafisa Patwary", "Dariusz Smola", "Tian Ying Chen", "Shakib Amin", "Noelle Wu", "Adib Miah", "Jason Teo"}, {"Siyam Siddique", "Leandra Boodram", "", "Rafael Bui", "Xin Chen", "Farian Faruqee"}, {"Irsal Siam", "Stanley Lee", "Sharon Lin", "", "Ricky Jiang", "Yanbo Wang"},{"", ""}, {"Christian Wan", "Brandon Yeung", "Nikita Manannikov", "Naya Antaki"}, {"", "Enamul Muttakin", "Jack Ni", "Ryan Chen"}};
+        ArrayList<Student> studentInfo = new ArrayList<Student>();
+        int seatNumber = 1;
+        for(int row = 0; row < seating.length; row++)
+        {
+            for(int column = 0; column < seating[row].length; column++)
+            {
+                if(!seating[row][column].equals(""))
+                {
+                    String name = seating[row][column];
+                    String leftNeighbor = "";
+                    if(column != 0)
+                    {
+                        leftNeighbor = seating[row][column - 1];
+                    }
+                    String rightNeighbor = "";
+                    if(column != (seating[row].length - 1))
+                    {
+                        rightNeighbor = seating[row][column + 1];
+                    }
+                    Student student = new Student(name, seatNumber, leftNeighbor, rightNeighbor);
+                    studentInfo.add(student);
+                    seatNumber++;
+                }
+            }
+        }
     }
 }
